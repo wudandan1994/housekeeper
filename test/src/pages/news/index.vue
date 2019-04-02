f3fa74a9bcc7f48d23e05c8394149e1cea3235f3
wudandan1994 <1058057286@qq.com>
2019-04-02 09:59:44 +0800

    news

---
+++ test/src/pages/news/index.vue
@@ -0,0 +1,66 @@
+<template>
+    <div id="news">
+        <header>
+            <span @click="goBack"><van-icon name="arrow-left"/></span>
+        </header>
+        <div class="container">
+            <van-tabs
+                padding-top="10px"
+                title-active-color="#F3C480"
+                background="#020202"
+                color="#F3C480"
+                title-inactive-color="#fff"
+                :swipeable="true"
+               >
+                    <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">
+                        内容 {{ index }}
+                    </van-tab>
+                </van-tabs>
+        </div>
+    </div>
+
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods:{
+        goBack() {
+            this.$router.push('/home')
+        }
+    }
+}
+</script>
+
+<style lang="less">
+   #news {
+       >header {
+           background: #000;
+           width:100%;
+           height: 46px;
+           line-height: 46px;
+           padding-top:10px;
+           color:#fff;
+           display: flex;
+           position: fixed;
+           justify-content: space-between;
+           >span {
+               &:nth-of-type(1) {
+                   margin-left: 10px;
+               }
+               &:nth-of-type(3) {
+                   margin-right: 10px;
+               }
+           }
+       }
+       >.container {
+           padding-top:56px;
+           padding-bottom: 50px;
+       }
+   }
+</style>
