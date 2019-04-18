2f6989ae2806c1432f5b05ea48b92b731797aff6
wudandan1994 <1058057286@qq.com>
2019-04-18 14:54:16 +0800

    list

--- test/src/pages/aisleHousekeeper/index.vue
+++ test/src/pages/aisleHousekeeper/index.vue
@@ -79,7 +79,11 @@ export default {
                         message:res.data.message
                     })
                 }
+                console.log(res);
+                
                 let list=JSON.parse(res.data.data.rt5_bindCardList)
+                console.log(list);
+                
                 if(list.length===0){
                       that.$toast=({
                         message:"你还没有绑定信用卡"
